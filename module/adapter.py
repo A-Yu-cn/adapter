8b2a0e3d66f7809dd7adfa231e4c0594e43d764a
马亮 <1791670972@qq.com>
2021-05-18 09:46:29 +0800

    fix #11203665: update message generator
    将消息生成从字符串拼接改为模板渲染

--- module/adapter.py
+++ module/adapter.py
@@ -2,6 +2,9 @@ from flask import request
 from flask_restful import Resource
 from module.send import Sender
 import json
+import logging
+from jinja2 import Template
+import os
 
 """
     Adapter 用于接收程序收到的各式请求，然后进行不同的处理
@@ -12,46 +15,54 @@ import json
 
 class Adapter(Resource):
 
-    @staticmethod
-    def post():
-        # 设置默认返回信息
-        repository = "未指定"
-        branch = "未指定"
-        commit_time = "now"
-        commit_info = "no info"
-        data = json.loads(request.data)
-        # print(data)
-        name = data.get("user_name")
-        project = data.get("project")
-        # 避免华为云平台推送的请求不符合规范导致以下字段为空
-        if project is not None:
-            repository = project.get("name")
-            branch = project.get("default_branch")
-        commits = data.get("commits")
-        if commits is not None:
-            commit_time = commits.get("timestamp")
-            commit_info = commits.get("message")
-        # 此下定义要返回给钉钉的信息
-        user_data = {
-            "msgtype": "markdown",
-            "markdown": {
-                "title": "代码检测",
-                "text": ""
-            },
-            "at": {
-                "isAtAll": 'true'
+    def __init__(self):
+        with open(os.environ.get("TEMPLATE_FILE"), 'r', encoding="utf8") as f:
+            template = f.read()
+        self.template = Template(template)
+
+    def post(self):
+        try:
+            # 设置默认返回信息
+            repository = "未指定"
+            branch = "未指定"
+            data = json.loads(request.data)
+            # print(data)
+            name = data.get("user_name")
+            project = data.get("project")
+            # 避免华为云平台推送的请求不符合规范导致以下字段为空
+            if project is not None:
+                repository = project.get("name")
+                branch = project.get("default_branch")
+            commits = data.get("commits")
+            commits_info = []
+            if isinstance(commits, list):
+                for commit in commits:
+                    commits_info.append({
+                        "message": commit.get("message")[:10],
+                        "url": commit.get("url")
+                    })
+            # 此下定义要返回给钉钉的信息
+            user_data = {
+                "msgtype": "markdown",
+                "markdown": {
+                    "title": "代码检测",
+                    "text": ""
+                }
             }
-        }
-        text = "**代码推送通知** \n  - 用户: {name} \n    推送到了 {repository} / {branch} 分支 \n  - commit信息: {commit_info} \n    " \
-               "at {commit_time}".format(name=name, repository=repository, branch=branch, commit_time=commit_time,
-                                         commit_info=commit_info)
-        user_data["markdown"]["text"] = text
-        # 暂时屏蔽推送到钉钉，部署服务器测试华为云平台发送消息是否正常
-        Sender.post(user_data)
-        # print(text)
-        return {
-            "result": "ok"
-        }
+            user_data["markdown"]["text"] = self.template.render({
+                "repository": repository,
+                "branch": branch,
+                "commits": commits_info,
+                "name": name
+            })
+            # 暂时屏蔽推送到钉钉，部署服务器测试华为云平台发送消息是否正常
+            Sender.post(user_data)
+            # print(text)
+            return {
+                "result": "ok"
+            }
+        except Exception as e:
+            logging.error(e)
 
     @staticmethod
     def get():
