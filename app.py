6deb2c5bdc91302e179aa688c57f0e49ee6e2136
马亮 <1791670972@qq.com>
2021-05-18 09:11:22 +0800

    fix #11203665:refactor code
    添加日志打印功能

--- app.py
+++ app.py
@@ -3,8 +3,16 @@ from module.adapter import Adapter
 from flask_restful import Api
 from dotenv import find_dotenv, load_dotenv
 import os
+import logging
 
 load_dotenv(find_dotenv(), encoding="utf8")
+
+# 日志设置
+log_file = os.environ.get("LOG_FILE")
+if log_file:
+    logging.basicConfig(filename=log_file)
+logging.basicConfig(format='[%(asctime)s](%(filename)s)  %(message)s', datefmt='%m/%d/%Y %H:%M:%S %p',
+                    level='INFO')
 
 app = Flask(__name__)
 api = Api(app)
